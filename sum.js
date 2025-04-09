b5b660e5b982ed559dbcfe6a6a72b26d4abd15d6
JoshSnyman333 <2677722@students.wits.ac.za>
2025-04-09 21:21:30 +0200

    CodeCov token fix 2

--- sum.js
+++ sum.js
@@ -1,5 +1,11 @@
+// sum.js
 function sum(a, b) {
     return a + b;
   }
-  module.exports = sum;
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  module.exports = { sum, subtract };
   

97afb688883aeacfce91eff720c58a1b1560099f
JoshSnyman333 <2677722@students.wits.ac.za>
2025-04-10 15:09:56 +0200

    Changed badge to track main

--- Jest.config.js
+++ Jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: [
-    '**/*.js',  // Includes all .js files in the project directory and subdirectories
+  collectCoverageFrom: [ //parts of code you want to check covered for
+    //'**/*.js',  // Includes all .js files in the project directory and subdirectories
+
+     'index.js',
+      'sum.js', // Include other files you want to cover
+
   ],
   coverageReporters: ["lcov", "text"],
 };

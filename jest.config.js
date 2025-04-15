8b19acd3cf0a21714323518a51654e9b9fb4ece6
JoshSnyman333 <2677722@students.wits.ac.za>
2025-04-15 20:39:48 +0200

    WIP: changes to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
-// jest.config.js
 module.exports = {
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-    testEnvironment: 'node',
-  };
-  
+  collectCoverage: true,
+  collectCoverageFrom: [ //parts of code you want to check covered for
+    //'**/*.js',  // Includes all .js files in the project directory and subdirectories
+
+     'index.js',
+      'sum.js', // Include other files you want to cover
+
+  ],
+  coverageReporters: ["lcov", "text"],
+};

9e93f19adc25aabf86a85b1bf50039a21382f4ec
Wallnutty <samsnyman801@gmail.com>
2025-05-02 16:01:15 +0200

    Fixing eslint warning

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -6,78 +6,79 @@ import { collection, getDocs } from "firebase/firestore";
 import { ref, getDownloadURL } from "firebase/storage";
 import { db, storage } from '../firebase/firebase';
 
+
+// Hardcoded products
+const hardcodedProducts = [
+  {
+    id: 'h1',
+    title: "poiandkeely",
+    image: "/keely.jpg",
+    description: "3d Modeling and Character Design, @poiandkeely",
+    color: '#FFEFF8',
+    textColor: '#A38FF7',
+    genre: '3D Modeling'
+  },
+  {
+    id: 'h2',
+    title: "Inio Asano",
+    image: "/Asano.jpg",
+    description: "Author and artist of 'Goodnight Punpun', 'Solanin' and 'A Girl On the Shore'.",
+    color: '#ffffff',
+    textColor: '#598EA0',
+    genre: 'Digital Art'
+  },
+  {
+    id: 'h3',
+    title: "조기석 Cho Gi-Seok",
+    image: "/Chogiseok.jpg",
+    description: "Korean photographer, director and artisan, 조기석 Cho Gi-Seok @chogiseok",
+    color: '#e7e4d7',
+    textColor: '#141118',
+    genre: 'Photography'
+  },
+  {
+    id: 'h4',
+    title: "Yusuke Murata",
+    image: "/Murata.gif",
+    description: "Artist of 'One Punch Man' and 'Eyeshield 21'.",
+    color: '#1e1e1e',
+    textColor: '#ffffff',
+    genre: 'Digital Art'	
+  },
+  {
+    id: 'h5',
+    title: "Inspired Island",
+    image: "/Island.jpg",
+    description: "Digital Media editor, artist and director, @CultureStudios ",
+    color: '#8C2C54',
+    textColor: '#FFDFE2',
+    genre:'Video Editing'
+  },
+  {
+    id: 'h6',
+    title: "Jamie Hewlett",
+    image: "/Hewlett.jpg",
+    description: "Digital Artist - @Hewll",
+    color: '#1c6e7b',
+    textColor: '#ffffff',
+    genre: 'Digital Art'
+  },
+  {
+    id: 'h7',
+    title: "Kim Jung Gi",
+    image: "/Kim.jpg",
+    description: "Physical inking artist and illustrator",
+    color: '#ffffff',
+    textColor: '#181818',
+    genre: 'Physical Art'
+  }
+];
+
 const Products = () => {
   // State to manage all products
   const [products, setProducts] = useState([]); // initially empty
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedGenre, setSelectedGenre] = useState('All');
-
-  // Hardcoded products
-  const hardcodedProducts = [
-    {
-      id: 'h1',
-      title: "poiandkeely",
-      image: "/keely.jpg",
-      description: "3d Modeling and Character Design, @poiandkeely",
-      color: '#FFEFF8',
-      textColor: '#A38FF7',
-      genre: '3D Modeling'
-    },
-    {
-      id: 'h2',
-      title: "Inio Asano",
-      image: "/Asano.jpg",
-      description: "Author and artist of 'Goodnight Punpun', 'Solanin' and 'A Girl On the Shore'.",
-      color: '#ffffff',
-      textColor: '#598EA0',
-      genre: 'Digital Art'
-    },
-    {
-      id: 'h3',
-      title: "조기석 Cho Gi-Seok",
-      image: "/Chogiseok.jpg",
-      description: "Korean photographer, director and artisan, 조기석 Cho Gi-Seok @chogiseok",
-      color: '#e7e4d7',
-      textColor: '#141118',
-      genre: 'Photography'
-    },
-    {
-      id: 'h4',
-      title: "Yusuke Murata",
-      image: "/Murata.gif",
-      description: "Artist of 'One Punch Man' and 'Eyeshield 21'.",
-      color: '#1e1e1e',
-      textColor: '#ffffff',
-      genre: 'Digital Art'	
-    },
-    {
-      id: 'h5',
-      title: "Inspired Island",
-      image: "/Island.jpg",
-      description: "Digital Media editor, artist and director, @CultureStudios ",
-      color: '#8C2C54',
-      textColor: '#FFDFE2',
-      genre:'Video Editing'
-    },
-    {
-      id: 'h6',
-      title: "Jamie Hewlett",
-      image: "/Hewlett.jpg",
-      description: "Digital Artist - @Hewll",
-      color: '#1c6e7b',
-      textColor: '#ffffff',
-      genre: 'Digital Art'
-    },
-    {
-      id: 'h7',
-      title: "Kim Jung Gi",
-      image: "/Kim.jpg",
-      description: "Physical inking artist and illustrator",
-      color: '#ffffff',
-      textColor: '#181818',
-      genre: 'Physical Art'
-    }
-  ];
 
   // Fetch artists from Firestore when the component loads
   useEffect(() => {
